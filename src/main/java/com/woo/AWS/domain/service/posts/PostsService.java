5df3d9acb6607ce410678a0204f9ea8b163a3c0f
심현우 <kshsim98@naver.com>
2024-07-13 06:52:03 +0900

    CRUD 완성

--- src/main/java/com/woo/AWS/domain/service/posts/PostsService.java
+++ src/main/java/com/woo/AWS/domain/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.woo.AWS.domain.service.posts;
 
 import com.woo.AWS.domain.posts.Posts;
 import com.woo.AWS.domain.posts.PostsRepository;
+import com.woo.AWS.web.dto.PostsListResponseDto;
 import com.woo.AWS.web.dto.PostsResponseDto;
 import com.woo.AWS.web.dto.PostsSaveRequestDto;
 import com.woo.AWS.web.dto.PostsUpdateRequestDto;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,18 @@ public class PostsService {
             .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findALlDesc() {
+        return postsRepository.findALLDesc().stream().map(PostsListResponseDto::new).collect(
+            Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+            IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+            postsRepository.delete(posts);
     }
 }
