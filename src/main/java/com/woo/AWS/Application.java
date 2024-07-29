bfc7d22d8baac4768c3de9015ef29d5c3005e3ae
심현우 <kshsim98@naver.com>
2024-07-29 22:13:18 +0900

    배포자동화구성

--- src/main/java/com/woo/AWS/Application.java
+++ src/main/java/com/woo/AWS/Application.java
@@ -1,6 +1,8 @@
 package com.woo.AWS;
+import javax.swing.Spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -8,6 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
         SpringApplication.run(Application.class, args);
     }
 
