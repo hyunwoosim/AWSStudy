2b8829cccf42df382271c6ee901b478004d91aa4
심현우 <kshsim98@naver.com>
2024-08-01 19:41:11 +0900

    >

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-
-ABSDIR=$(dirname $AVSPATH)
+ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo ">$IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
